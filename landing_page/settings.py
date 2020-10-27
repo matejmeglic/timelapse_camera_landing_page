89d1a3bd7014af0e5a33459dcbf3270c338e565e
matejmeglic <matej@matejmeglic.com>
2020-10-27 22:14:52 +0100

    PAGE LIVE

--- landing_page/settings.py
+++ landing_page/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 #  SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG_VALUE") == "True"
 
 ALLOWED_HOSTS = ["timelapsecamera.herokuapp.com"]
 
